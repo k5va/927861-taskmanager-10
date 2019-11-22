5bd3201d50828f35fe7389ca5a7c7d3f64259fd9
k5va <k5va@yandex.ru>
2019-11-22 15:04:52 +0300

    Added components

---
+++ src/components/board.js
@@ -0,0 +1,16 @@
+/**
+ * Creates board html template
+ * @return {String} template
+ */
+export const createBoardTemplate = () => {
+  return `<section class="board container">
+    <div class="board__filter-list">
+        <a href="#" class="board__filter">SORT BY DEFAULT</a>
+        <a href="#" class="board__filter">SORT BY DATE up</a>
+        <a href="#" class="board__filter">SORT BY DATE down</a>
+    </div>
+
+    <div class="board__tasks">
+    </div>
+   </section>`;
+};
--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,16 @@ const path = require(`path`);
 
 module.exports = {
   mode: `development`,
-  // Режим сборки
   entry: `./src/main.js`,
-  // Точка входа приложения
   output: {
-    // Настройка выходного файла
     filename: `bundle.js`,
     path: path.join(__dirname, `public`)
   },
-  devtool: `source-map`
+  devtool: `source-map`,
+  devServer: {
+    contentBase: path.join(__dirname, `public`),
+    publicPath: `http://localhost:8080/`,
+    compress: true,
+    watchContentBase: true
+  }
 };
