b1a139ebc0dd6322794c0b8f593dc57201fef957
k5va <k5va@yandex.ru>
2019-11-30 11:36:28 +0300

    Added MonthNames to const

--- src/const.js
+++ src/const.js
@@ -1,1 +1,16 @@
 export const Colors = [`black`, `yellow`, `blue`, `green`, `pink`];
+
+export const MonthNames = [
+  `January`,
+  `February`,
+  `March`,
+  `April`,
+  `May`,
+  `June`,
+  `July`,
+  `August`,
+  `September`,
+  `October`,
+  `November`,
+  `December`
+];
