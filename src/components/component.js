3213904d5f5637b85278ad1f44b4bc2bfac29d62
k5va <k5va@yandex.ru>
2019-12-04 23:57:43 +0300

    Added jsDocs to component

--- src/components/component.js
+++ src/components/component.js
@@ -1,15 +1,27 @@
 import {createElement} from "../utils/utils";
 
 export default class Component {
+  /**
+   * Component's costructor
+   * @param {String} template - HTML template string
+   */
   constructor(template) {
     this._element = null;
     this._template = template;
   }
 
+  /**
+   * Returns component's template
+   * @return {String} - template string
+   */
   getTemplate() {
     return this._template;
   }
 
+  /**
+   * Returns component's HTMLElement
+   * @return {HTMLElement} - HTMLElement that correspondes to component
+   */
   getElement() {
     if (!this._element) {
       this._element = createElement(this.getTemplate());
@@ -18,6 +30,9 @@ export default class Component {
     return this._element;
   }
 
+  /**
+   * Removes component from the DOM
+   */
   removeElement() {
     this.getElement().remove();
     this._element = null;
