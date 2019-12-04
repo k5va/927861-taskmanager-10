a260addb038ce340e3d51a484853052c55856126
k5va <k5va@yandex.ru>
2019-12-04 23:19:08 +0300

    Refactored main to use class components

--- src/main.js
+++ src/main.js
@@ -1,60 +1,52 @@
-import {createSiteMenuTemplate} from "./components/site-menu";
-import {createFilterTemplate} from "./components/filter";
-import {createBoardTemplate} from "./components/board";
-import {createTaskTemplate} from "./components/task";
-import {createLoadMoreTemplate} from "./components/load-more";
-import {createTaskFormTemplate} from "./components/task-form";
+import SiteMenu from "./components/site-menu";
+import Filter from "./components/filter";
+import Board from "./components/board";
+import Task from "./components/task";
+import LoadMore from "./components/load-more";
+import TaskForm from "./components/task-form";
 import {generateTasks} from "./mock/task";
 import {generateFilters} from "./mock/filter";
+import {render} from "./utils/utils";
 
 const TASK_COUNT = 22;
 const TASKS_PER_LOAD = 8;
-
-/**
- * Renders given HTML template to the DOM by adding it to the parent container
- * at the specified position
- * @param {HTMLElement} container - parent HTML element
- * @param {String} template - template to be added to the container
- * @param {String} place - insert position. Default value = "beforeend"
- */
-const render = (container, template, place = `beforeend`) => {
-  container.insertAdjacentHTML(place, template);
-};
 
 const mainElement = document.querySelector(`.main`);
 const controlElement = mainElement.querySelector(`.main__control`);
 const generatedTasks = generateTasks(TASK_COUNT);
 
 // render site menu
-render(controlElement, createSiteMenuTemplate());
+render(controlElement, new SiteMenu().getElement());
 // render filter
-render(mainElement, createFilterTemplate(generateFilters(generatedTasks)));
+render(mainElement, new Filter(generateFilters(generatedTasks)).getElement());
 // render board (tasks list)
-render(mainElement, createBoardTemplate());
+render(mainElement, new Board().getElement());
 
 const boardElement = mainElement.querySelector(`.board`);
 const tasksListElement = boardElement.querySelector(`.board__tasks`);
 
 // render add/edit task form
-render(tasksListElement, createTaskFormTemplate(generatedTasks[0]));
+render(tasksListElement, new TaskForm(generatedTasks[0]).getElement());
 // render tasks
-generatedTasks.slice(1, TASKS_PER_LOAD).forEach((task) => render(tasksListElement, createTaskTemplate(task)));
+generatedTasks
+  .slice(1, TASKS_PER_LOAD)
+  .forEach((task) => render(tasksListElement, new Task(task).getElement()));
 
+const loadMore = new LoadMore();
 // render load more button
-render(boardElement, createLoadMoreTemplate());
+render(boardElement, loadMore.getElement());
 
 let renderedTasksCount = TASKS_PER_LOAD;
-const loadMoreButton = document.querySelector(`.load-more`);
-loadMoreButton.addEventListener(`click`, () => {
+loadMore.getElement().addEventListener(`click`, () => {
   // render new portion of tasks
   generatedTasks.slice(renderedTasksCount, renderedTasksCount + TASKS_PER_LOAD)
-    .forEach((task) => render(tasksListElement, createTaskTemplate(task)));
+    .forEach((task) => render(tasksListElement, new Task(task).getElement()));
   // scroll to make load more button visible on page
-  loadMoreButton.scrollIntoView();
+  loadMore.getElement().scrollIntoView();
   // update rendered tasks counter and check if there are more tasks to load
   renderedTasksCount += TASKS_PER_LOAD;
   if (renderedTasksCount >= TASK_COUNT) {
     // no more tasks to load
-    loadMoreButton.remove();
+    loadMore.removeElement();
   }
 });
