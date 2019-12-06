f76bd926a88cb0941b0ade584f32bbc02585862e
k5va <k5va@yandex.ru>
2019-12-06 19:34:04 +0300

    Extracted template from load-more component

---
+++ src/components/load-more/template.js
@@ -0,0 +1,9 @@
+/**
+ * Creates load more button template
+ * @return {String} template
+ */
+const template = () => {
+  return `<button class="load-more" type="button">load more</button>`;
+};
+
+export {template};
