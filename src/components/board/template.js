003114419a8b758126eb242947cfa0f836cf5dff
k5va <k5va@yandex.ru>
2019-12-06 19:21:47 +0300

    Extracted template from board component

---
+++ src/components/board/template.js
@@ -0,0 +1,12 @@
+/**
+ * Creates board html template
+ * @return {String} template
+ */
+const template = () => {
+  return (
+    `<section class="board container">
+    </section>`
+  );
+};
+
+export {template};
