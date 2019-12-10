b1bec11ffcc5dd39eee51552728f94cb3672075a
k5va <k5va@yandex.ru>
2019-12-10 21:59:49 +0300

    Added setSortTypeChangeHandler to sort component

--- src/components/sort/index.js
+++ src/components/sort/index.js
@@ -1,12 +1,44 @@
 import AbstractComponent from "../component";
 import {template} from "./template";
+import {SortType} from "../../consts";
 
 export default class Sort extends AbstractComponent {
+
+  constructor() {
+    super();
+
+    this._currentSortType = SortType.DEFAULT;
+  }
+
   /**
    * Returns component's template
    * @return {String} - template
    */
   getTemplate() {
     return template();
+  }
+
+  /**
+   * Sets sort type change handler
+   * @param {Function} handler - handler
+   */
+  setSortTypeChangeHandler(handler) {
+    this.getElement().addEventListener(`click`, (evt) => {
+      evt.preventDefault();
+
+      if (!evt.target.classList.contains(`board__filter`)) {
+        return;
+      }
+
+      const sortType = evt.target.dataset.sortType;
+      if (this._currentSortType === sortType) {
+        // return if sort type hasn't changed
+        return;
+      }
+
+      // save current sort and call handler
+      this._currentSortType = sortType;
+      handler(sortType);
+    });
   }
 }
