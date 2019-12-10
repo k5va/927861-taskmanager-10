233b1ac06158f58c5222ae7f7fb8d11354e2c2a9
k5va <k5va@yandex.ru>
2019-12-10 19:53:45 +0300

    Refactored NoTasks component

--- src/components/no-tasks/index.js
+++ src/components/no-tasks/index.js
@@ -2,7 +2,11 @@ import AbstractComponent from "../component";
 import {template} from "./template";
 
 export default class NoTasks extends AbstractComponent {
-  constructor() {
-    super(template());
+  /**
+   * Returns component's template
+   * @return {String} - template
+   */
+  getTemplate() {
+    return template();
   }
 }
