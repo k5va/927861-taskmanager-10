31da6b1a883b65f772f1033681c608e3ec08145e
k5va <k5va@yandex.ru>
2019-12-10 19:56:54 +0300

    Refactored Tasks component

--- src/components/task-list/index.js
+++ src/components/task-list/index.js
@@ -2,7 +2,11 @@ import AbstractComponent from '../component';
 import {template} from "./template";
 
 export default class Tasks extends AbstractComponent {
-  constructor() {
-    super(template());
+  /**
+   * Returns component's template
+   * @return {String} - template
+   */
+  getTemplate() {
+    return template();
   }
 }
