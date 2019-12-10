508fb6fccb30f7ceb666f97ae4d8d8ca91526d36
k5va <k5va@yandex.ru>
2019-12-10 19:56:23 +0300

    Refactored Task form component

--- src/components/task-form/index.js
+++ src/components/task-form/index.js
@@ -3,6 +3,16 @@ import {template} from "./template";
 
 export default class TaskForm extends AbstractComponent {
   constructor(task) {
-    super(template(task));
+    super();
+
+    this._task = task;
+  }
+
+  /**
+   * Returns component's template
+   * @return {String} - template
+   */
+  getTemplate() {
+    return template(this._task);
   }
 }
