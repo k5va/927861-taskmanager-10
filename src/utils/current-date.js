375582729b6bd3c79f6c8287d2e2e9b9acc59ff2
k5va <k5va@yandex.ru>
2019-12-16 17:10:15 +0300

    Renamed isDateToday -> isCurrentDate

--- src/utils/current-date.js
+++ src/utils/current-date.js
@@ -1,6 +1,6 @@
 /**
- * Checks if date is due today
+ * Checks if date is current
  * @param {Date} date - date to be checked
  * @return {Boolean} - is date due today
  */
-export const isDateToday = (date) => date instanceof Date && date.toDateString() === new Date().toDateString();
+export const isCurrentDate = (date) => date instanceof Date && date.toDateString() === new Date().toDateString();
--- src/utils/index.js
+++ src/utils/index.js
@@ -5,7 +5,7 @@ export {getRandomBoolean} from "./random-boolean";
 export {formatDate} from "./format-date";
 export {formatTime} from "./format-time";
 export {isDateExpired} from "./date-expired";
-export {isDateToday} from "./current-date";
+export {isCurrentDate} from "./current-date";
 export {hasSomeBoolean} from "./has-some-boolean";
 export {createElement} from "./create-element";
 export {render, RenderPosition} from "./render";
