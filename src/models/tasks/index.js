e0826f835ef3282feac591bf30e76e366cf0c364
k5va <k5va@yandex.ru>
2019-12-18 16:24:24 +0300

    Added updateTask to Tasks model

--- src/models/tasks/index.js
+++ src/models/tasks/index.js
@@ -22,4 +22,23 @@ export default class Tasks {
   setTasks(tasks) {
     this._tasks = [...tasks];
   }
+
+  /**
+   * Updates task in model
+   *
+   * @param {String} id - task id
+   * @param {*} task - task object
+   * @return {Boolean} - true if model is successfully updated
+   */
+  updateTask(id, task) {
+    const index = this._tasks.findIndex((it) => it.id === id);
+
+    if (index === -1) {
+      return false;
+    }
+    // create new copy of tasks array with updated task. Can be used later for undo operations
+    this._tasks = [].concat(this._tasks.slice(0, index), task, this._tasks.slice(index + 1));
+
+    return true;
+  }
 }
