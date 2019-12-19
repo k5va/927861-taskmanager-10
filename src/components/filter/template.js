4e5e42c1a26dea46131fc9df66460920f156546a
k5va <k5va@yandex.ru>
2019-12-19 15:57:37 +0300

    Added disable to filters

--- src/components/filter/template.js
+++ src/components/filter/template.js
@@ -9,6 +9,7 @@ const template = (filters) => {
       `<input type="radio" id="filter__${name}"
         class="filter__input visually-hidden" name="filter" value="${name}"
         ${selected ? `checked` : ``}
+        ${count === 0 ? `disabled` : ``}
       />
       <label for="filter__${name}" class="filter__label">
         ${name}<span class="filter__${name}-count"> ${count}</span>
