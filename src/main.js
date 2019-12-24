c8df40742c7d74ae88b6f03869d9d9c90d04501e
k5va <k5va@yandex.ru>
2019-12-25 01:05:08 +0300

    Updated main to pass params to StatisticsComponent

--- src/main.js
+++ src/main.js
@@ -4,12 +4,15 @@ import {render} from "./utils";
 import {BoardController, FilterController} from "./controllers";
 import {TasksModel} from "./models";
 import {MenuItem} from "./consts";
+import moment from "moment";
 
 const TASK_COUNT = 22;
 
 const mainElement = document.querySelector(`.main`);
 const controlElement = mainElement.querySelector(`.main__control`);
 const tasks = generateTasks(TASK_COUNT);
+const endDate = new Date();
+const startDate = moment().subtract(7, `d`);
 const tasksModel = new TasksModel();
 tasksModel.setTasks(tasks);
 
@@ -25,7 +28,7 @@ render(mainElement, boardComponent);
 // create board controller and render tasks
 const boardController = new BoardController(boardComponent, tasksModel);
 // create statistics component and render
-const statisticsComponent = new StatisticsComponent();
+const statisticsComponent = new StatisticsComponent(tasksModel.getTasks(), startDate, endDate);
 render(mainElement, statisticsComponent);
 statisticsComponent.hide();
 
