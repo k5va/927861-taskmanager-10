a2f0003b2849fe4bbe920ee981cbfa7576ca4069
k5va <k5va@yandex.ru>
2020-01-01 20:59:49 +0300

    Added API

---
+++ src/api/check-status.js
@@ -0,0 +1,9 @@
+const checkStatus = (response) => {
+  if (response.status >= 200 && response.status < 300) {
+    return response;
+  } else {
+    throw new Error(`${response.status}: ${response.statusText}`);
+  }
+};
+
+export {checkStatus};
---
+++ src/api/index.js
@@ -0,0 +1,82 @@
+import {Task} from "../models";
+import {checkStatus} from "./check-status";
+
+const Method = {
+  GET: `GET`,
+  POST: `POST`,
+  PUT: `PUT`,
+  DELETE: `DELETE`
+};
+
+export default class API {
+
+  constructor(endPoint, authorization) {
+    this._endPoint = endPoint;
+    this._authorization = authorization;
+  }
+
+  /**
+   * Get tasks list from server
+   * @return {Promise<Array<Task>>} - promise that resolves with array of tasks
+   */
+  getTasks() {
+    return this._send({url: `tasks`})
+      .then((response) => response.json())
+      .then(Task.parseTasks);
+  }
+
+  /**
+   * Creates new task on server
+   * @param {Task} task - task
+   * @return {Promise<Task>} - promise that resolves to created task
+   */
+  createTask(task) {
+    return this._send({
+      url: `tasks`,
+      method: Method.POST,
+      body: JSON.stringify(task.toRAW()),
+      headers: new Headers({'Content-Type': `application/json`})
+    })
+      .then((response) => response.json())
+      .then(Task.parseTask);
+  }
+
+  /**
+   * Updates task on server
+   * @param {Task} task - task
+   * @return {Promise<Task>} - promise that resolves to updated task
+   */
+  updateTask(task) {
+    return this._send({
+      url: `tasks/${task.id}`,
+      method: Method.PUT,
+      body: JSON.stringify(task.toRAW()),
+      headers: new Headers({'Content-Type': `application/json`})
+    })
+      .then((response) => response.json())
+      .then(Task.parseTask);
+  }
+
+  /**
+   * Deletes task on server
+   * @param {String} id - task id
+   * @return {Promise<Response>} - promise that resoves to server response if it is successfull
+   */
+  deleteTask(id) {
+    return this._send({url: `tasks/${id}`, method: Method.DELETE});
+  }
+
+  /**
+   * Sends request to server
+   * @return {Promise<Response>} - promise that resoves to server response if it is successfull
+   */
+  _send({url, method = Method.GET, body = null, headers = new Headers()}) {
+    headers.append(`Authorization`, this._authorization);
+
+    return fetch(`${this._endPoint}/${url}`, {method, body, headers})
+      .then(checkStatus)
+      .catch((err) => {
+        throw err;
+      });
+  }
+}
