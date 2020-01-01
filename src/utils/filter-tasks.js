c4a564bed03ec615085cb205414638117fb545fb
k5va <k5va@yandex.ru>
2020-01-02 00:42:09 +0300

    Fixed FilterTasks to exclude archive tasks

--- src/utils/filter-tasks.js
+++ src/utils/filter-tasks.js
@@ -3,10 +3,10 @@ import {Filter} from "../consts";
 
 export const FilterTasks = {
   [Filter.ALL]: (tasks) => tasks.filter(({isArchive}) => !isArchive),
-  [Filter.OVERDUE]: (tasks) => tasks.filter(({dueDate}) => isDateExpired(dueDate)),
-  [Filter.TODAY]: (tasks) => tasks.filter(({dueDate}) => isCurrentDate(dueDate)),
-  [Filter.FAVORITES]: (tasks) => tasks.filter(({isFavorite}) => isFavorite),
-  [Filter.REPEATING]: (tasks) => tasks.filter(({repeatingDays}) => hasSomeBoolean(repeatingDays)),
-  [Filter.TAGS]: (tasks) => tasks.filter(({tags}) => tags.size > 0),
+  [Filter.OVERDUE]: (tasks) => tasks.filter(({isArchive, dueDate}) => !isArchive && isDateExpired(dueDate)),
+  [Filter.TODAY]: (tasks) => tasks.filter(({isArchive, dueDate}) => !isArchive && isCurrentDate(dueDate)),
+  [Filter.FAVORITES]: (tasks) => tasks.filter(({isArchive, isFavorite}) => !isArchive && isFavorite),
+  [Filter.REPEATING]: (tasks) => tasks.filter(({isArchive, repeatingDays}) => !isArchive && hasSomeBoolean(repeatingDays)),
+  [Filter.TAGS]: (tasks) => tasks.filter(({isArchive, tags}) => !isArchive && tags.size > 0),
   [Filter.ARCHIVE]: (tasks) => tasks.filter(({isArchive}) => isArchive)
 };
